0d78de0fc2832ca1f16cf99c147959d2b21dd71c
mfardeen9 <mfardeen9@gmail.com>
2022-12-12 17:41:53 +0530

    added the add a note functionality

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -4,10 +4,12 @@ const Noteitem = (props) => {
     const { note } = props;
     return (
         <div className='col-md-3'>
-            <div class="card my-3">
-                <div class="card-body">
-                    <h5 class="card-title">{note.title}</h5>
-                    <p class="card-text">{note.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus veniam alias cupiditate, aperimpedit ipsum ad, harum consectetur laboriosam, voluptates ea et! Quis animi aliquid </p>
+            <div className="card my-3">
+                <div className="card-body">
+                    <h5 className="card-title">{note.title}</h5>
+                    <p className="card-text">{note.description}  </p>
+                    <i className="fa-solid fa-trash-can "></i>
+                    <i className="fa-regular fa-pen-to-square mx-4"></i>
                    
                 </div>
             </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,11 +3,12 @@ import { useState } from "react";
 import NoteContext from "./noteContext"
 
 import React from 'react'
+import Notes from "../../components/Notes";
 
 function NoteState(props) {
     const noteInitial =[
         {
-          "_id": "638baa91831d640cdbdd3d73",
+          "_id": "638baa91831d6401cdbdd3d73",
           "user": "638a41cab660096b94232a72",
           "title": "Fardeen is Great! ",
           "description": "FARDEEN ZINDABAD ",
@@ -16,7 +17,7 @@ function NoteState(props) {
           "__v": 0
         },
         {
-          "_id": "638bab93a8dd82237e11dae0",
+          "_id": "638bab93a8dd8223q17e11dae0",
           "user": "638a41cab660096b94232a72",
           "title": "Fardeen is Great!",
           "description": "FARDEEN ZINDABAD is so great",
@@ -25,7 +26,7 @@ function NoteState(props) {
           "__v": 0
         },
         {
-          "_id": "638baa91831d640cdbdd3d73",
+          "_id": "638baa91831d640cdwb1dd3d73",
           "user": "638a41cab660096b94232a72",
           "title": "Fardeen is Great! ",
           "description": "FARDEEN ZINDABAD ",
@@ -34,7 +35,7 @@ function NoteState(props) {
           "__v": 0
         },
         {
-          "_id": "638bab93a8dd82237e11dae0",
+          "_id": "638bab93a8dd8223e7e11d1ae0",
           "user": "638a41cab660096b94232a72",
           "title": "Fardeen is Great!",
           "description": "FARDEEN ZINDABAD is so great",
@@ -44,7 +45,7 @@ function NoteState(props) {
         }
         ,
         {
-          "_id": "638baa91831d640cdbdd3d73",
+          "_id": "638ba1a91831d640cgdbdd3d73",
           "user": "638a41cab660096b94232a72",
           "title": "Fardeen is Great! ",
           "description": "FARDEEN ZINDABAD ",
@@ -52,39 +53,35 @@ function NoteState(props) {
           "date": "2022-12-03T19:59:13.449Z",
           "__v": 0
         },
-        {
-          "_id": "638bab93a8dd82237e11dae0",
-          "user": "638a41cab660096b94232a72",
-          "title": "Fardeen is Great!",
-          "description": "FARDEEN ZINDABAD is so great",
-          "tag": "question",
-          "date": "2022-12-03T20:03:31.425Z",
-          "__v": 0
-        }
-      ,
-        {
-          "_id": "638baa91831d640cdbdd3d73",
-          "user": "638a41cab660096b94232a72",
-          "title": "Fardeen is Great! ",
-          "description": "FARDEEN ZINDABAD ",
-          "tag": "question up",
-          "date": "2022-12-03T19:59:13.449Z",
-          "__v": 0
-        },
-        {
-          "_id": "638bab93a8dd82237e11dae0",
-          "user": "638a41cab660096b94232a72",
-          "title": "Fardeen is Great!",
-          "description": "FARDEEN ZINDABAD is so great",
-          "tag": "question",
-          "date": "2022-12-03T20:03:31.425Z",
-          "__v": 0
-        }
+       
+        
       ]
     const [notes, setNotes] =useState(noteInitial);
+
+    //Add a Note
+      const addNote =(title, description,tag)=>{
+        const note ={
+          "_id": "638bab93a8dxd82237eyyyy11da1e0",
+          "user": "638a41cab660096b94t2312a72",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2022-12-03T20:03:31.425Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note))
+      }
+    //Delete a Note
+    const deleteNote =()=>{
+        
+    }
+    //Edit a Note
+    const editNote =()=>{
+        
+    }
   return (
     
-   <NoteContext.Provider value={{notes,setNotes}}>
+   <NoteContext.Provider value={{notes,setNotes, addNote,deleteNote,editNote}}>
     {props.children}
    </NoteContext.Provider>
   )
